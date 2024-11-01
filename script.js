692306bfb1c105ec000cfb169854051fff9af76f
Fahrurroji <ojikura123@gmail.com>
2024-11-01 04:45:06 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.gallery-img').forEach(img => {
+    img.addEventListener('click', function() {
+        document.getElementById('modal-photo').src = this.src;
+    });
+});
